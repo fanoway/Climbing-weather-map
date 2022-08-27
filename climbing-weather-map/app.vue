fe70f5a4402e0845a7600e9cb2d45070af3aa116
Michael Ardron <MY_NAME@example.com>
2022-08-27 14:41:29 -0700

    bulma added

--- climbing-weather-map/app.vue
+++ climbing-weather-map/app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+  <button class = "button">Hello World</button>
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
+
+
+ 
